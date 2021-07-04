ebaffc3fe7d489e1fd345f7e05eec58477a64379
Joshua Oren <joshuadavidoren@gmail.com>
2021-07-03 20:24:54 -0500

    fixed typos

--- database.sql
+++ database.sql
@@ -6,10 +6,10 @@ CREATE TABLE to_do(
 
 INSERT INTO "to_do" ( "task", "complete" )
 VALUES ('Win the pod race', FALSE),
-('free my mom from slavery', FALSE),
-('become a Jedi', FALSE),
+('Free my mom from slavery', FALSE),
+('Become a Jedi', FALSE),
 ('Be the best pilot in the Galaxy', FALSE),
 ('Become powerful enough to prevent Death and divide by 0', FALSE),
-('Kill all of the Jedi starting with the younglings (they taste the best)',	FALSE),
+('Kill all of the Jedi starting with the younglings',	FALSE),
 ('Save Padme', FALSE),
-('definitely do not kill Padme', FALSE);
+('Definitely do not kill Padme', FALSE);
